177bdc473002d01bcc58b0606070cf0a911e8034
ssmith129 <smith.design530@gmail.com>
2025-04-11 14:28:25 -0700

    v1

---
+++ main/src/app/(DashboardLayout)/dashboards/dashboard1/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+import React from "react";
+import { useEffect, useState } from "react";
+
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid2";
+import PageContainer from "@/app/components/container/PageContainer";
+
+// components
+import CongratulationsCard from "@/app/components/dashboards/dashboard1/CongratulationsCard";
+import Payments from "@/app/components/dashboards/dashboard1/Payments";
+import Products from "@/app/components/dashboards/dashboard1/Products";
+import LatestDeals from "@/app/components/dashboards/dashboard1/LatestDeals";
+import Customers from "@/app/components/dashboards/dashboard1/Customers";
+import ProductTable from "@/app/components/dashboards/dashboard1/ProductTable";
+import VisitUsa from "@/app/components/dashboards/dashboard1/VisitUsa";
+import LatestReviews from "@/app/components/dashboards/dashboard1/LatestReviews";
+import Welcome from "@/app/(DashboardLayout)/layout/shared/welcome/Welcome";
+
+export default function Dashboard() {
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  return (
+    (<PageContainer title="Dashboard" description="this is Dashboard">
+      <Box>
+        <Grid container spacing={3}>
+          <Grid
+            size={{
+              xs: 12,
+              lg: 3
+            }}>
+            <Grid container spacing={3}>
+              <Grid
+                size={{
+                  xs: 12,
+                  sm: 6,
+                  lg: 12
+                }}>
+                <Payments isLoading={isLoading} />
+              </Grid>
+              <Grid
+                size={{
+                  xs: 12,
+                  sm: 6,
+                  lg: 12
+                }}>
+                <Products isLoading={isLoading} />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            size={{
+              xs: 12,
+              lg: 6
+            }}>
+            <CongratulationsCard isLoading={isLoading} />
+          </Grid>
+          <Grid
+            size={{
+              xs: 12,
+              lg: 3
+            }}>
+            <Grid container spacing={3}>
+              <Grid
+                size={{
+                  xs: 12,
+                  lg: 12,
+                  sm: 6
+                }}>
+                <LatestDeals />
+              </Grid>
+              <Grid
+                size={{
+                  xs: 12,
+                  lg: 12,
+                  sm: 6
+                }}>
+                <Customers isLoading={isLoading} />
+              </Grid>
+            </Grid>
+          </Grid>
+          {/* column */}
+          <Grid
+            size={{
+              xs: 12,
+              lg: 4
+            }}>
+            <VisitUsa />
+          </Grid>
+          <Grid
+            size={{
+              xs: 12,
+              lg: 8
+            }}>
+            <ProductTable />
+          </Grid>
+          
+          <Grid size={12}>
+            <LatestReviews />
+          </Grid>
+        </Grid>
+        <Welcome />
+      </Box>
+    </PageContainer>)
+  );
+}
---
+++ main/src/app/components/frontend-pages/shared/team/Contact.tsx
@@ -0,0 +1,70 @@
+"use client";
+import React from "react";
+import {
+  Box,
+  Stack,
+  Typography,
+  Link,
+  AvatarGroup,
+  Container,
+  Avatar,
+} from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+const Contact = () => {
+  //   sidebar
+  const lgUp = useMediaQuery((theme: any) => theme.breakpoints.up("lg"));
+
+  return (
+    <Box
+      bgcolor="primary.main"
+      borderRadius={0}
+      textAlign="center"
+      py="14px"
+      position="relative"
+    >
+      <Container maxWidth="lg">
+        <Stack
+          direction={{ xs: "column", sm: "row" }}
+          spacing="16px"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <AvatarGroup>
+            <Avatar
+              alt="Remy Sharp"
+              src="/images/profile/user-1.jpg"
+              sx={{ width: 40, height: 40, border: `2px solid white !important` }}
+            />
+            <Avatar
+              alt="Travis Howard"
+              src="/images/profile/user-2.jpg"
+              sx={{ width: 40, height: 40, border: `2px solid white !important` }}
+            />
+          </AvatarGroup>
+          <Typography variant="body1" color="white" fontSize="16px">
+            Save valuable time and effort spent searching for a solution.
+          </Typography>
+          <Link
+            href="/"
+            underline="always"
+            sx={{
+              textDecorationColor: "white",
+            }}
+          >
+            <Typography
+              component="span"
+              fontWeight={600}
+              color="white"
+              fontSize="16px"
+            >
+              Contact us now
+            </Typography>
+          </Link>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Contact;
