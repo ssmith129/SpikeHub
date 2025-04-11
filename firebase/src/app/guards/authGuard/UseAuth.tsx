2e56e64ddbae0e3246dc17d7cbbd76983dc6c8e5
ssmith129 <smith.design530@gmail.com>
2025-04-11 14:53:41 -0700

    v10

--- firebase/src/app/guards/authGuard/UseAuth.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import AuthContext from '../firebase/FirebaseContext';
-
-//import { AuthContext } from '../jwt/JwtContext';
-//import { AuthContext } from '../auth0/Auth0Context';
-
-const useAuth = () => useContext(AuthContext);
-
-export default useAuth;
